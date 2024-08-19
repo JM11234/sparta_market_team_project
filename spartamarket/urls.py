ac3948906cbd52bd9fd9d5a2bb91f9776a14b06d
leeyounghuncom <web3dao@kakao.com>
2024-08-19 12:09:16 +0900

    상품쪽 빠진거 적용

--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -19,4 +19,11 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #첫화면은 상품리스트
+    path('', include('products.urls')),
+    #사용자
+    path('accounts/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
