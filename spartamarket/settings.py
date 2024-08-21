413467a507ece89a34e45c5ee3d0a5ba59e664b5
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-08-21 09:00:22 +0900

    hit-count 설치

--- spartamarket/settings.py
+++ spartamarket/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'django.contrib.humanize',
+    'hitcount'
 
 ]
 
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'spartamarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #첫화면은 상품리스트
+    path('', include('products.urls')),
+    #사용자
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
